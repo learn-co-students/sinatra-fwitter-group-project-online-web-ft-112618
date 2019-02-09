49640a1949d15bc72468724d36f5e27e52f7055e
Nick Inman <nick@theoatmeal.com>
2019-02-09 17:42:56 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,63 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if is_logged_in?(session)
+      @tweets = Tweet.all
+      erb :'/tweets/index'
+    else
+      redirect '/users/login'
+    end
+  end
 
+  get '/tweets/new' do
+    if is_logged_in?(session)
+      erb :'/tweets/new'
+    else
+      redirect '/users/login'
+    end
+  end
+
+  post '/tweets/new' do
+    if is_logged_in?(session) && params[:content] != ""
+      #binding.pry
+      @tweet = Tweet.new(:content => params[:content], :user_id => session[:user_id])
+      @tweet.save
+      redirect '/tweets'
+    else
+      redirect '/tweets/new'
+    end
+  end
+
+  get '/tweets/:id' do
+    if is_logged_in?(session)
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if is_logged_in?(session)
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/edit_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/tweets/:id/edit' do
+      @tweet = Tweet.find(params[:id])
+      @tweet.update(:content => params[:content])
+  end
+
+  delete '/tweets/:id' do
+      @tweet = Tweet.find(params[:id])
+      if @tweet.user_id == current_user(session).id && is_logged_in?(session)
+        @tweet.delete
+        redirect '/tweets'
+      else
+        redirect '/tweets'
+      end
+    end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  has_many :tweets
+  has_secure_password
+
+  def slug
+    self.username.split(' ').join('-')
+  end
+
+  def self.find_by_slug(user_slug)
+    self.all.find{ |user| user.slug == user_slug}
+  end
+end
