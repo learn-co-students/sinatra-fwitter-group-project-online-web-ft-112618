bc1e11421046154a4a17ed0264f9e0504edc7e32
Molly McCarron <mccarronmollye@gmail.com>
2019-05-14 20:17:59 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,30 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb ':users/create_user'
+  end
+
+
+
+
+# check who is the current user
+def current_user
+  user = ''
+end
+# if we don't have an user who logged in
+# if nil, direct user to login page
+def logged_in?
+  !!current_user
+end
+
+#if user not loggin in, run this to check if we have this user
+# if return nil, direct user to signup page
+def user_exist?
+end
+
 end
